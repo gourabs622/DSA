725d60d31b4d303d11e95d58cdaae340f985f516
GOURAB SARKAR <57136496+gourabs622@users.noreply.github.com>
2021-12-09 17:37:08 +0530

    Add files via upload

---
+++ Data-Structures/graph/DFS_nonDi.cpp
@@ -0,0 +1,179 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+
+/*          					GOURAB SARKAR					            */
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define PI 3.141592653589793238
+#define MP make_pair
+#define F first
+#define S second
+#define PB push_back
+#define PBO pop_back()
+#define permute next_permutation
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define ALLA(arr, sz) arr, arr + sz
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define dl "\n"
+#define mm(a, val) memset(a, val, sizeof(a))
+#define b_s binary_search
+#define u_b upper_bound
+#define l_b lower_bound
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define fast()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const ld EPS = 1e-9;
+const int N = 1e6 + 7;
+const ll NL = 1000000000000000005LL;
+const int mod = 1e9 + 7;
+const int mod1 = 1e9 + 9;
+const int mod2 = 998244353;
+
+#define fi(a, b, c) for (int a = b; a < c; ++a)
+#define fd(a, b, c) for (int a = c - 1; a >= b; --a)
+#define fei(a, b, c) for (int a = b; a <= c; ++a) //for any particular range
+#define fed(a, b, c) for (int a = c; a >= b; --a) //for any particular range
+#define fs(a, b) for (auto &a : b)                //for loop shortcut
+
+// inline void OPEN()
+// {
+//     #ifdef ONLINE_JUDGE
+//         freopen("input.in", "r", stdin);
+//         freopen("output.in", "w", stdout);
+//     #endif
+// }
+
+//Graph
+//Adj Matrix
+const int maxN = 1e4 + 1, maxM = 1e4 + 1;
+int adjt[maxN][maxM];
+vector<bool> vist(maxN, false);
+
+//Adj List
+vector<int> adj[N];
+vector<bool> vis(N, false);
+
+inline void dfsAM(int i, int n)
+{
+    if (vist[i] == false)
+    {
+        cout << i << " ";
+        vist[i] = true;
+        fi(j, 0, n)
+        {
+            if (adjt[i][j] == 1 && vist[j] == false)
+            {
+                dfsAM(j, n);
+            }
+        }
+    }
+}
+
+inline void dfsAL(int i)
+{
+    if (vis[i] == false)
+    {
+        cout << i << " ";
+        vis[i] = true;
+        // q.push(i);
+        for (auto it = adj[i].begin(); it != adj[i].end(); it++)
+        {
+            if (vis[*it] == false)
+            {
+                dfsAL(*it);
+            }
+        }
+    }
+}
+
+inline void AM() //for adjacency matrix
+{
+    int n, e;
+    cin >> n >> e;
+    fi(i, 0, e)
+    {
+        int u, v;
+        cin >> u >> v;
+        adjt[u][v] = 1;
+        adjt[v][u] = 1;
+    }
+    // fi(i, 0, n)
+    // {
+    //     fi(j, 0, n)
+    //     {
+    //         cout << adjt[i][j] << " ";
+    //     }
+    //     cout << dl;
+    // }
+    dfsAM(0, n);
+    cout << dl;
+    fi(i, 0, maxN)
+    {
+        fi(j, 0, maxM)
+        {
+            adjt[i][j] = 0;
+        }
+        vist[i] = false;
+    }
+}
+
+inline void AL() //for Adjacency List
+{
+    int n, e;
+    cin >> n >> e;
+    fi(i, 0, e)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].PB(v);
+        adj[v].PB(u);
+    }
+    // fi(i, 0, n)
+    // {
+    //     for (auto it = adj[i].begin(); it != adj[i].end(); it++)
+    //     {
+    //         cout << *it << " ";
+    //     }
+    //     cout << dl;
+    // }
+    dfsAL(0);
+    cout << dl;
+    fi(i, 0, N)
+    {
+        adj[i].clear();
+        vis[i] = false;
+    }
+}
+
+inline void solve()
+{
+    fast();
+    // AM();
+    AL();
+}
+
+int main()
+{
+    fast();
+    // OPEN();
+    // solve();
+    test
+    {
+        solve();
+    }
+
+    return 0;
+}
